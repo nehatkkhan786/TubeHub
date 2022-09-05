724a94ecdc3c55108204eb4c5848da9cb4a2d993
nehatkkhan786 <33215298+nehatkkhan786@users.noreply.github.com>
2022-09-05 18:28:34 +0530

    sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,34 @@
+import { Stack, Typography, Box, Paper, IconButton } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import { SearchOutlined } from '@mui/icons-material';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <Stack direction="row"
+    alignItems='center'
+    p={2}
+    sx={{position:'sticky', background:'#000', top:0, justifyContent:'space-between'}}
+    >
+
+      <Box sx={{display:{xs:'none',md:'flex' }, alignItems:'center', justifyContent:'center', cursor:'pointer',}}>
+         <PlayCircleFilledIcon sx={{fontSize:40, color:'orange', display:'flex'}} /> 
+         <Typography sx={{color:'white', ml:1}}>TubeHub</Typography>
+      </Box>
+      
+      <Paper component='form'
+      sx={{borderRadius:20, border:'1px solid #e3e3e3', pl:2, boxShadow:'none'}}
+      >
+          <input
+          className="search-bar"
+          placeholder="Search...."
+          />
+          <IconButton>
+            <SearchOutlined sx={{color:'orange', p:'10px'}}/>
+          </IconButton>
+      </Paper>
+   </Stack>
   )
 }
 
